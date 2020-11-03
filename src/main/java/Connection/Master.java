37ddf4230b2a0f20adbd1de67093456133a6d500
Vitalii <danilov.vit.an@gmai.com>
2020-11-03 16:05:46 +0700

    Prototype Log Message

--- src/main/java/Connection/Master.java
+++ src/main/java/Connection/Master.java
@@ -1,5 +1,6 @@
-package Modbus;
+package Connection;
 
+import Model.DataModbus;
 import com.intelligt.modbus.jlibmodbus.Modbus;
 import com.intelligt.modbus.jlibmodbus.ModbusMaster;
 import com.intelligt.modbus.jlibmodbus.exception.ModbusIOException;
@@ -10,22 +11,36 @@ import com.intelligt.modbus.jlibmodbus.serial.*;
 import jssc.SerialPortList;
 
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Master {
     private static SerialParameters serialParameters;
     private static ModbusMaster master;
-    private static int slaveID = 1,
-                       addressOne = 430,
-                       addressTwo = 700,
-                       quality = 14;
+    private static DataModbus dm = new DataModbus();
+
+    public static int inputRegister(int x){
+        try {
+            master.writeMultipleRegisters(dm.getSlaveID(), dm.getAddressOne(), new int[]{x});
+            System.out.println("Write: " + x);
+        } catch (ModbusProtocolException | ModbusNumberException | ModbusIOException e) {
+            e.getMessage();
+        }
+        return x;
+    }
+
+    public static void outputRegister(){
+        try {
+            System.out.println("Data output "
+                    + Arrays.toString(master.readInputRegisters(dm.getSlaveID(), dm.getAddressTwo(), dm.getQuality())));
+        } catch (ModbusProtocolException | ModbusNumberException | ModbusIOException e) {
+            e.getMessage();
+        }
+    }
 
     public static void main(String[] args) {
-        serialParameters = new SerialParameters();
-        Modbus.setLogLevel(Modbus.LogLevel.LEVEL_DEBUG);
         try{
-
+            serialParameters = new SerialParameters();
+            Modbus.setLogLevel(Modbus.LogLevel.LEVEL_DEBUG);
+            Modbus.setLogLevel(Modbus.LogLevel.LEVEL_WARNINGS);
             String[] dev_list = SerialPortList.getPortNames();
             if (dev_list.length > 0) {
                 serialParameters.setDevice(dev_list[0]);
@@ -37,28 +52,21 @@ public class Master {
 
             SerialUtils.setSerialPortFactory(new SerialPortFactoryJSSC());
             master = new ModbusMasterRTU(serialParameters);
+
             master.setResponseTimeout(1000);
 
             master.connect();
 
-            Queue<O> queue = new LinkedList<ModbusMaster>();
 
-
-            for (int i = 0; i < 200; i++){
-                master.writeMultipleRegisters(slaveID, addressOne, new int[]{i});
-                System.out.println("Data input " + i + " "
-                        + Arrays.toString(master.readInputRegisters(slaveID, addressTwo, quality)));
+            for (int i = 0; i <= 200; i++){
+                inputRegister(i);
+                Thread.sleep(1000);
+                outputRegister();
                 Thread.sleep(1000);
             }
-
             master.disconnect();
-        } catch(RuntimeException |
-                ModbusIOException |
-                SerialPortException |
-                ModbusNumberException |
-                ModbusProtocolException |
-                InterruptedException e){
-            e.printStackTrace();
+        } catch(RuntimeException | ModbusIOException | SerialPortException | InterruptedException e){
+            e.getMessage();
         }
     }
 }
---
+++ src/main/java/Model/DataModbus.java
@@ -0,0 +1,21 @@
+package Model;
+
+public class DataModbus {
+    private int slaveID, addressOne, addressTwo, quality;
+
+    public int getSlaveID() {
+        return 1;
+    }
+
+    public int getAddressOne() {
+        return 430;
+    }
+
+    public int getAddressTwo() {
+        return 700;
+    }
+
+    public int getQuality() {
+        return 14;
+    }
+}
